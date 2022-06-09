b4ae3e95cd1addb09b6200b9c5bf1bbd6dd826bd
shahazad08 <sk.shahazad@gmail.com>
2022-06-09 12:45:36 +0530

    Added: Even Odd program added

---
+++ src/com/bridgelabz/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+       // System.out.println("Hello World");
+        int number=13;
+        //Check if the number is divisible by 2 or not
+        if(number%2==0){
+            System.out.println("even number");
+        }else{
+            System.out.println("odd number");
+        }
+    }
+}
